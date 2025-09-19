e7103c44c175f30f6d03a5fdaf46eb9cb7cb7b46
alishukur2 <yaalishukur@gmail.com>
2025-09-19 10:20:49 +0400

    main files and folders added

---
+++ lang-of-dp/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import style from './mainPage.module.css';
+
+
+const MainPage = () => {
+    return (
+        <h1>hello</h1>
+    );
+};
+
+export default MainPage;
